a7cc08032898c29cb4007d074be27c35c3ab0e8c
DESKTOP-1Q9TKRP\HP <rakib33mbstu@gmail.com>
2023-03-12 12:45:10 +0600

    [add Interface for role service]

---
+++ UserManagementCore/UserManagementCore/Interfaces/IApplicationRoleService.cs
@@ -0,0 +1,14 @@
+﻿using UserManagementCore.Models;
+
+namespace UserManagementCore.Interfaces
+{
+    public interface IApplicationRoleService
+    {
+        public Task<IEnumerable<ApplicationRole>> GetOnlyRoleList();
+        public Task<IEnumerable<ApplicationRole>> GetRoleList();
+        public Task<IEnumerable<ApplicationRole>> GetRolesWithDetails();
+        public Task<IEnumerable<ApplicationRole>> GetRolesWithUsers();
+        public Task<IEnumerable<ApplicationRole>> GetRolesWithClaims();
+        public Task<ApplicationRole> GetRole(string roleId, string roleName);
+    }
+}
