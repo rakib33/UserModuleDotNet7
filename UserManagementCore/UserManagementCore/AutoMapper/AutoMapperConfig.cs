86490ecc344ac12c4ab5e3652ad732487b2f37b6
DESKTOP-1Q9TKRP\HP <rakib33mbstu@gmail.com>
2023-03-12 11:48:56 +0600

    [Add AutoMepper and Common/AppStatus(AppVariable is now Appstatus)]

---
+++ UserManagementCore/UserManagementCore/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using UserManagementCore.Models;
+using UserManagementEntityModel.ViewModel;
+using AutoMapper;
+namespace UserManagementCore.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {           
+            CreateMap<ApplicationUser,vm_ApplicationUser>().ReverseMap();
+        }
+    }
+}
---
+++ UserManagementCore/UserManagementCore/Common/AppStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementCore.Common
+{
+    public class AppStatus
+    {
+            public static string Role_Admin = "Admin";
+            public static string SuccessStatus = "Success";
+            public static string FailedStatus = "Failed";
+            public static string ErrorStatus = "Error";        
+    }
+}
