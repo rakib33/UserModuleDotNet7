17e510621bbf39a2b871264b619a1163d4c73840
Rakibul Islam <islam.rakibul@bjitgroup.com>
2023-03-14 17:30:47 +0600

    [Services added]

---
+++ UserManagementCore/UserManagementCore/Interfaces/IApplicationRoleDetailsService.cs
@@ -0,0 +1,13 @@
+﻿using UserManagementCore.Models;
+
+namespace UserManagementCore.Interfaces
+{
+    public interface IApplicationRoleDetailsService
+    {
+        public IEnumerable<ApplicationRoleDetails> GetRoleById(int id);
+        public IEnumerable<ApplicationRoleDetails> GetAllApplicationRoleDetailss();
+        public Task<ApplicationRoleDetails> AddApplicationRoleDetails(ApplicationRoleDetails applicationRoleDetails);
+        public bool DeleteApplicationRoleDetails(int id);
+        public bool UpdateApplicationRoleDetails(List<ApplicationRoleDetails> DataList);
+    }
+}
--- UserManagementCore/UserManagementCore/Interfaces/IRepository.cs
+++ UserManagementCore/UserManagementCore/Interfaces/IRepository.cs
@@ -1,6 +1,17 @@
 ﻿namespace UserManagementCore.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T>
     {
+        public Task<T> Create(T _object);
+
+        public void Update(T _object);
+
+        public IEnumerable<T> GetAll();
+
+        public T GetById(int Id);
+
+        public void Delete(T _object);
+
     }
+
 }
