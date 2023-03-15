ab46909ecdf53ef38f78f8063bf795980f2d1dfc
DESKTOP-1Q9TKRP\HP <rakib33mbstu@gmail.com>
2023-03-16 00:13:57 +0600

    [Add TDD project and do sample RouteService Test]

---
+++ UserManagementCore/UserManagementCore.Tests/ApplicationRouteServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagementCore.Models;
+using UserManagementCore.Repositories;
+
+namespace UserManagementCore.Tests
+{
+    public class ApplicationRouteServiceTests
+    {
+        [Fact]
+        public void ShouldReturnAppRouteResultWithRequestValues()
+        {
+            var processor = new ApplicationRouteServices();
+            ApplicationMenu request = new ApplicationMenu { 
+              MenuName= "app",
+              Id= 1
+            };
+            // Act  
+            ApplicationMenu response = processor.CreateApplicationRoute(request);
+
+            // Assert  
+            Assert.NotNull(response);
+            Assert.Equal(request.MenuName, response.MenuName);
+            Assert.Equal(request.Id, response.Id);
+        
+        }
+    }
+}
---
+++ UserManagementCore/UserManagementCore/Repositories/ApplicationRouteServices.cs
@@ -0,0 +1,15 @@
+﻿using UserManagementCore.Models;
+
+namespace UserManagementCore.Repositories
+{
+    public class ApplicationRouteServices
+    {
+
+        public ApplicationRouteServices() { 
+        }
+        public ApplicationMenu CreateApplicationRoute(ApplicationMenu applicationMenu) 
+        {
+            return applicationMenu;
+        }
+    }
+}
