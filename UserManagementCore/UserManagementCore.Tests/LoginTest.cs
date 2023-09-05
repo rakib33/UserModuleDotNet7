b2053e44277c20b25be3d1e47c98d97aefc830c9
rakibul islam <islam.rakibul@bjitgroup.com>
2023-09-05 17:32:51 +0600

    login test

---
+++ UserManagementCore/UserManagementCore.Tests/LoginTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagementCore.Controllers;
+using UserManagementCore.Interfaces;
+
+namespace UserManagementCore.Tests
+{
+    public class LoginTest
+    {
+        private readonly Mock<IHttpContextAccessor> _accessor;
+        private readonly LoginsController _loginsController;
+        private readonly Mock<ILogins> _logins;
+
+        public LoginTest()
+        {
+            _logins = new Mock<ILogins>();
+            var session = Mock.Of<ISession>();
+            var httpContext = Mock.Of<HttpContext>(x => x.Session == session);
+            _accessor = new Mock<IHttpContextAccessor>();
+            _accessor.Setup(x => x.HttpContext).Returns(httpContext);
+        }
+
+     
+        [Fact]
+        public void Index_ReturnLoginViewModel() 
+        { 
+         var result = (_loginsController.Index() as ViewResult);
+            Assert.NotNull(result);
+            Assert.Equal("Login",result.ViewName,ignoreCase:true);
+        }
+    }
+}
